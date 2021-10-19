cd4c6ef70ace5f2c9fe63a350029738ca11647c6
zayson <jymaeng95@gmail.com>
2021-10-19 21:03:55 +0900

    [feat] First Commit

---
+++ user/src/main/java/com/yourecipe/user/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.yourecipe.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())            //Swagger API 문서로 만들기 원하는 basePackage 경로
+                .paths(PathSelectors.any())    //URL 경로를 지정하여 해당 URL에 해당하는 요청만 SWAGGER로 만듦
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("YouRecipe User API")
+                .description("YouRecipe User API Reference for Developers")
+                .termsOfServiceUrl("https://github.com/jymaeng95/YouRecipe-User")
+                .license("@Zayson License")
+                .licenseUrl("http://you-recipe.com").version("1.0").build();
+    }
+}
